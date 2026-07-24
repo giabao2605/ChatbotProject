b5145d2b3b40669a4ade44129a55c77d652e0e07
Gia Bao <blam12237@gmail.com>
2026-07-24 08:49:07 +0700

    test: refresh phase four golden corpus

--- tests/conftest.py
+++ tests/conftest.py
@@ -39,7 +39,7 @@ def pytest_collection_modifyitems(config, items):
                 item.add_marker(skip_qdrant)
             elif not needs_qdrant and not run_db:
                 item.add_marker(skip_db)
-        if "eval" in item.keywords and not run_eval:
+        if item.get_closest_marker("eval") is not None and not run_eval:
             item.add_marker(skip_eval)
 
 
